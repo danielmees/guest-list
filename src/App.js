80237e8ebee824e136b71dd4859d9511d30fab7b
Bin Mi <danielmees2014@gmail.com>
2019-11-16 22:18:28 +1100

    load guests data into page and add styling and responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 import axios from 'axios';
+import GuestCard from './components/GuestCard';
 
 class App extends Component {
   state = {
@@ -26,8 +27,20 @@ class App extends Component {
   }
 
   render() {
+    const { loading, guests, error } = this.state;
+    
     return (
-      <div className="App">
+      <div className="container">
+        <h1>Guests of Cafe Masa</h1>
+        <div className='guests-card-container'>
+          {loading && <h3>Loading, please wait...</h3>}
+          {(loading && error) && <h3>{error}</h3>}
+          {(!loading && guests.length > 0) &&
+            guests.map(guest => 
+              <GuestCard key={guest.id} guest={guest} />
+            )
+          }
+        </div>
       </div>
     );
   }
---
+++ src/components/GuestCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './GuestCard.scss';
+import diningIcon from '../assets/dining.png';
+
+const GuestCard = ({ guest }) => {
+  const { first_name, last_name, email, city, visit_count, total_spend, tags } = guest;
+
+  return (
+    <div className='guest-card'>
+      <img src={diningIcon} alt='guest icon' />
+      <div className='guest-info'>
+        <p className='name'>{first_name} {(last_name) ? last_name : ''}</p>
+        <ul className='tags'>
+          {tags && tags.map((tag, index) =>
+            <li key={index}>{tag}</li>
+          )}
+        </ul>
+        <div className='email-city'>
+          <p className='email'>{(email) ? email : ''}</p>
+          <p className='city'>{(city) ? city : ''}</p>
+        </div>
+        <p className='visit-info'>Has visited {visit_count} times, total spend is ${total_spend}</p>
+      </div>
+    </div>
+  );
+}
+
+export default GuestCard;
