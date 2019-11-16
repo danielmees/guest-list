583e422238c0be66cbcf0a90ccfeb5c0320b4c6d
Bin Mi <danielmees2014@gmail.com>
2019-11-16 19:03:05 +1100

    fetch guest data from page load and unit test case update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: true,
+    guests: [],
+    error: false
+  };
+
+  componentDidMount() {
+    return axios.get('http://localhost:3000/db/guests.json')
+      .then(res => {
+        this.setState({
+          loading: false,
+          guests: res.data.data
+        });
+      })
+      .catch(() => {
+        this.setState({
+          loading: false,
+          error: 'Oops, something went wrong!'
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,109 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import axios from 'axios';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+jest.mock('axios', () => {
+  const fakeResponse = {
+    data: {
+      data:[
+        {
+          id: 1,
+          first_name: "Byrom",
+          last_name: "Leggen",
+          email: null,
+          city: "Chat Trakan",
+          visit_count: 7,
+          total_spend: 137.71,
+          allow_marketing: true,
+          tags: ["protocol", "migration", "executive", "Down-sized", "Proactive"]
+        },
+        {
+          id: 2,
+          first_name: "Lilias",
+          last_name: "Pace",
+          email: "lpace1@toplist.cz",
+          city: "Krasnyye Chetai",
+          visit_count: 22,
+          total_spend: 138.03,
+          allow_marketing: true,
+          tags: ["protocol", "Compatible", "Cloned", "Cloned", "frame"]
+        },
+        {
+         id: 3,
+         first_name: "Nappy",
+         last_name: "Prate",
+         email: "nprate2@g.co",
+         city: "Funza",
+         visit_count: 14,
+         total_spend: 153.27,
+         allow_marketing: false,
+         tags: [
+            "client-driven",
+            "encompassing",
+            "Vision-oriented",
+            "6th generation",
+            "Down-sized"
+          ]
+        }
+      ]
+    }
+  };
+  return {
+    get: jest.fn(() => Promise.resolve(fakeResponse)),
+  };
+});
+
+const guests = [
+  {
+    id: 1,
+    first_name: "Byrom",
+    last_name: "Leggen",
+    email: null,
+    city: "Chat Trakan",
+    visit_count: 7,
+    total_spend: 137.71,
+    allow_marketing: true,
+    tags: ["protocol", "migration", "executive", "Down-sized", "Proactive"]
+  },
+  {
+    id: 2,
+    first_name: "Lilias",
+    last_name: "Pace",
+    email: "lpace1@toplist.cz",
+    city: "Krasnyye Chetai",
+    visit_count: 22,
+    total_spend: 138.03,
+    allow_marketing: true,
+    tags: ["protocol", "Compatible", "Cloned", "Cloned", "frame"]
+  },
+  {
+   id: 3,
+   first_name: "Nappy",
+   last_name: "Prate",
+   email: "nprate2@g.co",
+   city: "Funza",
+   visit_count: 14,
+   total_spend: 153.27,
+   allow_marketing: false,
+   tags: [
+      "client-driven",
+      "encompassing",
+      "Vision-oriented",
+      "6th generation",
+      "Down-sized"
+    ]
+  }
+];
+
+it('fetches guests information on #componentDidMount and update states correctly', () => {
+  const wrapper = shallow(<App />);
+  wrapper.instance()
+    .componentDidMount()
+    .then((response) => {
+      expect(axios.get).toHaveBeenCalled();
+      expect(axios.get).toHaveBeenCalledWith('http://localhost:3000/db/guests.json');
+      expect(wrapper.state('loading')).toEqual(false);
+      expect(wrapper.state('guests')).toEqual(guests);
+    });
 });
